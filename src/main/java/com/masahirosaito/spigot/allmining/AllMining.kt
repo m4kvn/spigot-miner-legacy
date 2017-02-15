1494b4fb636a140db222f0a3d43f8847e70f0c07
MasahiroSaito <MasahiroSaito@users.noreply.github.com>
2017-02-15 21:55:35 +0900

    [Add] Update check

--- src/main/java/com/masahirosaito/spigot/allmining/AllMining.kt
+++ src/main/java/com/masahirosaito/spigot/allmining/AllMining.kt
@@ -1,15 +1,23 @@
 package com.masahirosaito.spigot.allmining
 
+import com.masahirosaito.spigot.mscore.Messenger
+import com.masahirosaito.spigot.mscore.UpdateChecker
 import org.bukkit.plugin.java.JavaPlugin
 
 class AllMining : JavaPlugin() {
+    lateinit var messenger: Messenger
 
     override fun onEnable() {
-        // Plugin startup logic
+        messenger = Messenger(this, true)
 
+        checkUpdate()
     }
 
     override fun onDisable() {
         // Plugin shutdown logic
+    }
+
+    private fun checkUpdate() {
+        UpdateChecker("masahirosaito-repo", "Spigot-Plugin", "AllMining").sendVersionMessage(this)
     }
 }
