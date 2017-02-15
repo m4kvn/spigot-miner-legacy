026db76a4186b6d1ee8c3f9b7395b73e9dbf3551
MasahiroSaito <MasahiroSaito@users.noreply.github.com>
2017-02-16 01:05:00 +0900

    [Add] Configs Data Class

---
+++ src/main/java/com/masahirosaito/spigot/allmining/Configs.kt
@@ -0,0 +1,25 @@
+package com.masahirosaito.spigot.allmining
+
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import com.google.gson.annotations.SerializedName
+import java.io.File
+
+data class Configs(
+
+        @SerializedName("採掘時にデータを表示する")
+        val onMiningData: Boolean = true,
+
+        @SerializedName("スニーキング状態の時のみ採掘する")
+        val onSneaking: Boolean = false
+) {
+    companion object {
+        fun load(file: File): Configs {
+            return Gson().fromJson(file.readText().let {
+                if (it.isNullOrBlank()) Configs().toJson() else it
+            }, Configs::class.java).apply { file.writeText(toJson()) }
+        }
+    }
+
+    private fun toJson(): String = GsonBuilder().setPrettyPrinting().create().toJson(this)
+}
