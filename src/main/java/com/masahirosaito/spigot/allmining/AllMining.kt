ff2cd488f235a1d1cacd7a8f62c42846620b5989
MasahiroSaito <MasahiroSaito@users.noreply.github.com>
2017-02-15 21:45:08 +0900

    [Init] Gradle Project

---
+++ src/main/java/com/masahirosaito/spigot/allmining/AllMining.kt
@@ -0,0 +1,15 @@
+package com.masahirosaito.spigot.allmining
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class AllMining : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
