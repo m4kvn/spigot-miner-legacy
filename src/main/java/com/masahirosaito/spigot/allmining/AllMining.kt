0c1392708666204a10ea4c449c01c1b9070a5fa7
MasahiroSaito <MasahiroSaito@users.noreply.github.com>
2017-02-15 22:59:56 +0900

    [Edit] get NMS instance method

--- src/main/java/com/masahirosaito/spigot/allmining/AllMining.kt
+++ src/main/java/com/masahirosaito/spigot/allmining/AllMining.kt
@@ -1,14 +1,19 @@
 package com.masahirosaito.spigot.allmining
 
+import com.masahirosaito.spigot.allmining.nms.NMS
+import com.masahirosaito.spigot.allmining.nms.NMS_V1_11_R1
+import com.masahirosaito.spigot.allmining.nms.NMS_v1_10_R1
 import com.masahirosaito.spigot.mscore.Messenger
 import com.masahirosaito.spigot.mscore.UpdateChecker
 import org.bukkit.plugin.java.JavaPlugin
 
 class AllMining : JavaPlugin() {
     lateinit var messenger: Messenger
+    lateinit var nms: NMS
 
     override fun onEnable() {
         messenger = Messenger(this, true)
+        nms = getNMS()
 
         checkUpdate()
     }
@@ -19,5 +24,11 @@ class AllMining : JavaPlugin() {
 
     private fun checkUpdate() {
         UpdateChecker("masahirosaito-repo", "Spigot-Plugin", "AllMining").sendVersionMessage(this)
+    }
+
+    private fun getNMS(): NMS = when (server.bukkitVersion) {
+        "1.10.2-R0.1-SNAPSHOT" -> NMS_v1_10_R1()
+        "1.11.2-R0.1-SNAPSHOT" -> NMS_V1_11_R1()
+        else -> throw Exception()
     }
 }
