cb0d185a53d2d07fa32e1793253b4a3944d5bfcf
MasahiroSaito <MasahiroSaito@users.noreply.github.com>
2017-02-20 01:13:36 +0900

    [Edit] Permission がなかった場合にメッセージを送信

--- src/main/java/com/masahirosaito/spigot/allmining/commands/AMCommand.kt
+++ src/main/java/com/masahirosaito/spigot/allmining/commands/AMCommand.kt
@@ -1,12 +1,14 @@
 package com.masahirosaito.spigot.allmining.commands
 
 import com.masahirosaito.spigot.allmining.AllMining
+import org.bukkit.ChatColor
 import org.bukkit.command.Command
 import org.bukkit.command.CommandExecutor
 import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
 
-class AMCommand(plugin: AllMining) : CommandExecutor {
+class AMCommand(val plugin: AllMining) : CommandExecutor {
+    val permission = "allmining.command"
     val subCommands = listOf(OnCommand(plugin), OffCommand(plugin))
 
     override fun onCommand(sender: CommandSender?, command: Command?,
@@ -14,14 +16,27 @@ class AMCommand(plugin: AllMining) : CommandExecutor {
 
         if (sender !is Player) return true
 
+        if (!sender.hasPermission(permission)) return sendPermissionMsg(sender, permission)
+
         if (args == null || args.isEmpty()) return true
 
-        subCommands.find { it.name == args[0]}?.let {
+        subCommands.find { it.name == args[0] }?.let {
             if (it.hasPermission(sender)) {
                 it.execute(sender, args.drop(1))
+            } else {
+                return sendPermissionMsg(sender, it.permission)
             }
         }
 
+        return true
+    }
+
+    fun sendPermissionMsg(player: Player, permission: String): Boolean {
+        plugin.messenger.send(player, buildString {
+            append(ChatColor.RED)
+            append("You don't have permission <$permission>")
+            append(ChatColor.RESET)
+        })
         return true
     }
 }
