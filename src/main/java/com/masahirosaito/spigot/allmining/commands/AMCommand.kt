ea973a83081b6ebfb2677d2958bae78e1d2a858c
MasahiroSaito <MasahiroSaito@users.noreply.github.com>
2017-02-20 00:58:51 +0900

    [Edit] am コマンドを SubCommand を使った処理に変更

--- src/main/java/com/masahirosaito/spigot/allmining/commands/AMCommand.kt
+++ src/main/java/com/masahirosaito/spigot/allmining/commands/AMCommand.kt
@@ -1,39 +1,27 @@
 package com.masahirosaito.spigot.allmining.commands
 
 import com.masahirosaito.spigot.allmining.AllMining
-import org.bukkit.ChatColor
 import org.bukkit.command.Command
 import org.bukkit.command.CommandExecutor
 import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
 
 class AMCommand(plugin: AllMining) : CommandExecutor {
-    val playerdata = plugin.playerdata
-    val messenger = plugin.messenger
+    val subCommands = listOf(OnCommand(plugin), OffCommand(plugin))
 
     override fun onCommand(sender: CommandSender?, command: Command?,
                            label: String?, args: Array<out String>?): Boolean {
 
         if (sender !is Player) return true
 
-        if (args == null || args.isEmpty()) return false
+        if (args == null || args.isEmpty()) return true
 
-        return when (args[0]) {
-            "on" -> on(sender)
-            "off" -> off(sender)
-            else -> false
+        subCommands.find { it.name == args[0]}?.let {
+            if (it.hasPermission(sender)) {
+                it.execute(sender, args.drop(1))
+            }
         }
-    }
 
-    fun on(player: Player): Boolean {
-        playerdata.data.put(player.uniqueId, true)
-        messenger.send(player, "${ChatColor.RED}AllMining ON${ChatColor.RESET}")
-        return true
-    }
-
-    fun off(player: Player): Boolean {
-        playerdata.data.put(player.uniqueId, false)
-        messenger.send(player, "${ChatColor.BLUE}AllMining OFF${ChatColor.RESET}")
         return true
     }
 }
